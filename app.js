cba05eab788913ab4287556dd0ea0c7599f0bd12
sonsj97 <sonsj97@naver.com>
2025-01-02 00:59:25 +0900

    fix: swagger endpoint 변경

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const { swaggerSpec, basicAuth } = require('./swagger');
 const app = express();
 
 app.use(express.json());
-app.use('/api-docs', basicAuth, require('swagger-ui-express').serve, require('swagger-ui-express').setup(swaggerSpec));
+app.use('/look/api-docs', basicAuth, require('swagger-ui-express').serve, require('swagger-ui-express').setup(swaggerSpec));
 
 app.use('/api', carRoutes);
 app.use(errorHandler);
