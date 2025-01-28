58d5f28639a245c20036f83cabe4905049e44e3c
SonAIengine <sonsj97@gmail.com>
2025-01-28 15:38:02 +0900

    fix: verify 로그 반영, secure를 true로 변경

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -28,6 +28,7 @@ exports.loginController = async (req, res, next) => {
         httpOnly: true, // 클라이언트 JS에서 접근 불가
         secure: true,
         maxAge: 3600000, // 쿠키 만료 시간: 1시간 (밀리초)
+        sameSite: "None", // 크로스 도메인 요청 허용
         path: "/", // 모든 경로에서 쿠키 사용 가능
       });
 
