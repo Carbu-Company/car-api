e333d01f780df62ac30309f0e3e893d5b6fd7215
SonAIengine <sonsj97@gmail.com>
2025-02-09 19:38:18 +0900

    fix: cors

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const { swaggerSpec, basicAuth } = require("./swagger");
 const app = express();
 
 // 허용할 도메인 배열
-const whitelist = ["http://115.68.193.63:3000", "http://carbu.infoedu.co.kr", "http://localhost:3000", "http://carbu-api.infoedu.co.kr];
+const whitelist = ["http://115.68.193.63:3000", "http://carbu.infoedu.co.kr", "http://localhost:3000", "http://carbu-api.infoedu.co.kr"]
 
 // CORS 설정
 const corsOptions = {
